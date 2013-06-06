cffb045735b370848f17f47079d56f20e010769f
Jonathan Twaddell <jon@arclocal.com>
2013-06-06 12:31:38 -0700

    ini

---
+++ StoryboardRecipes/PRPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PRPAppDelegate.h
+//  StoryboardRecipes
+//
+//  Created by Jonathan Twaddell on 6/6/13.
+//  Copyright (c) 2013 Jonathan Twaddell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PRPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StoryboardRecipes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StoryboardRecipes
+//
+//  Created by Jonathan Twaddell on 6/6/13.
+//  Copyright (c) 2013 Jonathan Twaddell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PRPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PRPAppDelegate class]));
+  }
+}
